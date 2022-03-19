4eb8cbc699fb9e481adc5076137fd200852fad75
Lkarion <97668616+Lkarion@users.noreply.github.com>
2022-03-19 23:14:02 +0100

    Update AddressGetter.java

--- src/com/company/AddressGetter.java
+++ src/com/company/AddressGetter.java
@@ -3,11 +3,7 @@ package com.company;
 public class AddressGetter {
     //Create the third class AddressGetter which accepts an array
     //of persons and returns an array of their addresses
-    Person[] persons;
-
-    public AddressGetter(Person[] persons) {
-        this.persons = persons;
-    }
+    
     public static Address[] getAddressArray(Person[] persons){
         Address[] addresses = new Address[persons.length];
         for (int i = 0; i < persons.length; i++) {
